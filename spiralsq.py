1548294c68b5e13b3ca568447514cee8a7313ffe
Sparsh Rastogi <sparshrastogi5880@gmail.com>
2022-04-21 00:17:54 +0530

    Add files via upload

---
+++ spiralsq.py
@@ -0,0 +1,49 @@
+import turtle
+
+wn=turtle.Screen()
+
+wn.bgcolor("black")
+
+skk=turtle.Turtle()
+
+skk.color("white")
+
+def sqrfunc(size):
+    for i in range(4):
+        skk.fd(size)
+
+        skk.left(90)
+
+        size=size+5
+
+sqrfunc(6)
+sqrfunc(26)
+sqrfunc(46)
+sqrfunc(66)
+sqrfunc(86)
+sqrfunc(106)
+sqrfunc(126)
+sqrfunc(146)
+sqrfunc(166)
+sqrfunc(186)
+sqrfunc(206)
+sqrfunc(226)
+sqrfunc(246)
+sqrfunc(266)
+sqrfunc(286)
+sqrfunc(306)
+sqrfunc(326)
+sqrfunc(346)
+sqrfunc(366)
+sqrfunc(386)
+sqrfunc(406)
+sqrfunc(426)
+sqrfunc(446)
+sqrfunc(466)
+sqrfunc(486)
+sqrfunc(506)
+sqrfunc(526)
+sqrfunc(546)
+sqrfunc(566)
+sqrfunc(586)
+sqrfunc(606)
